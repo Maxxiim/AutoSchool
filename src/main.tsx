33694a29c95b0d364bae8ccc68db16973336f927
Safronov <Safronovmaxiim@mail.ru>
2025-10-17 21:02:55 +0300

    fixed provider

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/autoSchool">
       <App />
     </BrowserRouter>
   </StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   base: "/AutoSchool/",
   build: {
-    outDir: "pages",
+    outDir: "dist",
     emptyOutDir: true,
   },
 });
